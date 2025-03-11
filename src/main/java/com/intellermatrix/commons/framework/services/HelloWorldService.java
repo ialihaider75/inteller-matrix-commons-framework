bb69416b288e0165968ecd5155ce8e23f6f0a27b
ialihaider75 <ialihaider75@gmail.com>
2025-03-12 00:08:50 +0500

    Implemented a simple HelloWorldService

---
+++ src/main/java/com/intellermatrix/commons/framework/services/HelloWorldService.java
@@ -0,0 +1,9 @@
+package com.intellermatrix.commons.framework.services;
+
+
+public class HelloWorldService {
+
+    public String getHelloWorld() {
+        return "Hello World! from commons-framework of Intellermatrix";
+    }
+}
